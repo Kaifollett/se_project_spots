16f6860a2f8405bedacf2b8f319bcd594a728167
KaiFollett <kaifollett@live.com>
2025-04-23 02:09:00 -0600

    final update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -28,8 +28,20 @@ const initialCards = [
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_opened");
 });
 
@@ -37,9 +49,21 @@ editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_opened");
 });
 
+function handleEditProfileSubmit(event) {
+  event.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_opened");
+}
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostCaptionInput = document.querySelector("#card-caption-input");
+const newPostImageInput = document.querySelector("#card-image-input");
+
+const newPostFormEl = document.querySelector("#new-post-modal .modal__form");
 
 newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_opened");
@@ -48,3 +72,13 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_opened");
 });
+
+function handleAddCardSubmit(event) {
+  event.preventDefault();
+
+  console.log("Caption:", newPostCaptionInput.value);
+  console.log("Image URL:", newPostImageInput.value);
+  newPostModal.classList.remove("modal_opened");
+}
+
+newPostFormEl.addEventListener("submit", handleAddCardSubmit);
